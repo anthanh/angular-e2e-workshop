bf7a0ab3121ea7c4d31d9bc669881af86398131f
Anthanh Pham Trinh <antai.pt@gmail.com>
2018-04-25 09:16:59 +0200

    dataHook locator addded

--- e2e/tariffdeatail/tariffdeatail.po.ts
+++ e2e/tariffdeatail/tariffdeatail.po.ts
@@ -1,10 +1,15 @@
 import { browser, ExpectedConditions, element, by } from 'protractor';
 
+by.addLocator('dataHook', function (hook, optParentElement, optRootSelector) {
+  const using = optParentElement || document.querySelector(optRootSelector) || document;
+  return using.querySelector('[data-hook=\'' + hook + '\']');
+});
+
 export class TariffDetail {
 
-  readyElement = '.left-content thor-button';
-  title = 'thor-tariff-landing h1';
-  mobileData = '.tariff-info .overview-list li:first-child strong';
+  readyElement = 'readyElement';
+  title = 'title';
+  mobileData = 'mobileData';
 
   constructor(private tariffPath: string) { }
 
@@ -14,17 +19,17 @@ export class TariffDetail {
 
   waitUntilReady() {
     return browser.wait(
-      ExpectedConditions.visibilityOf(element(by.css(this.readyElement))),
+      ExpectedConditions.visibilityOf(element(by.dataHook(this.readyElement))),
       15000
     );
   }
 
   getTitle() {
-    return element(by.css(this.title)).getText();
+    return element(by.dataHook(this.title)).getText();
   }
 
   getMobileData() {
-    return element(by.css(this.mobileData)).getText();
+    return element(by.dataHook(this.mobileData)).getText();
   }
 
 }
