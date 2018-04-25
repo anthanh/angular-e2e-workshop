8449bad7aa3051b0d1043eb5a0278821603b23a0
Anthanh Pham Trinh <antai.pt@gmail.com>
2018-04-25 09:04:15 +0200

    spec implementation

--- e2e/tariffdeatail/tariffdetail.e2e-spec.ts
+++ e2e/tariffdeatail/tariffdetail.e2e-spec.ts
@@ -1,11 +1,34 @@
+import { browser } from 'protractor';
+import { TariffDetail } from './tariffdeatail.po';
+
+
+const tariffName = 'fibra-optica/gigas-infinitos-fibra-300mb';
 
 describe('Tariff Detail', () => {
+  let page: TariffDetail;
+
+  beforeAll(async () => {
+    // Ensure defined screen size
+    await browser.driver.manage().window().setSize(1280, 1024);
+  });
 
   describe('with infinita tariff', () => {
 
-    it('title is correct', async () => { });
+    beforeEach(async () => {
+      page = new TariffDetail(tariffName);
+      await page.navigateTo();
+      await page.waitUntilReady();
+    });
 
-    it('mobiledata is correct', async () => { });
+    it('title is correct', async () => {
+      const title = await page.getTitle();
+      expect(title.toLowerCase()).toBe('gigas infinitos en tu móvil y 300 mb de fibra');
+    });
+
+    it('mobiledata is correct', async () => {
+      const mobileData = await page.getMobileData();
+      expect(mobileData.toLowerCase()).toBe('gb infinit∞s');
+    });
 
   });
 
