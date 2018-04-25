24c2ebe171654302ae3171a0c6b48fb312f3c0b8
Anthanh Pham Trinh <antai.pt@gmail.com>
2018-04-25 08:55:52 +0200

    spec definition

---
+++ e2e/tariffdeatail/tariffdetail.e2e-spec.ts
@@ -0,0 +1,12 @@
+
+describe('Tariff Detail', () => {
+
+  describe('with infinita tariff', () => {
+
+    it('title is correct', async () => { });
+
+    it('mobiledata is correct', async () => { });
+
+  });
+
+});
